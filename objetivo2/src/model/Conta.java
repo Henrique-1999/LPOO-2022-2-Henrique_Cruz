7139742103a68ed4b8218cc51b39b281b25cac58
Henrique-1999 <henriquecruz53@gmail.com>
2022-11-27 22:02:51 -0300

    teste

--- objetivo2/src/model/Conta.java
+++ objetivo2/src/model/Conta.java
@@ -1,4 +1,52 @@
 package model;
 
-public class Conta {
+public abstract class Conta {
+
+    protected int id;
+
+    protected double saldo;
+
+    public void deposita(double valor){
+        saldo = saldo + valor;
+        System.out.println("Conta " +  id + ":\n" + "Novo saldo: " + saldo );
+    }
+
+    public void saca(double valor) {
+        if (saldo >= valor) {
+            saldo = saldo - valor;
+            System.out.println("Conta " +  id + ":\n" +  "Novo saldo: " + saldo);
+        }
+        else {
+            System.out.println( "Conta " +  id + ":\n" + "Saldo insuficiente para realizar saque");
+        }
+    }
+
+    public void atualiza(double taxa){
+        saldo = saldo + saldo * taxa;
+        System.out.println( "Conta " +  id + ":\n" + "Novo saldo: " + saldo );
+    }
+
+    public Conta() {
+    }
+
+    public Conta(int id, double saldo) {
+        this.id = id;
+        this.saldo = saldo;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    @Override
+    public String toString() {
+        return "Conta{" +
+                "id=" + id +
+                ", saldo=" + saldo +
+                '}';
+    }
 }
--- objetivo2/src/model/ContaCorrente.java
+++ objetivo2/src/model/ContaCorrente.java
@@ -1,4 +1,38 @@
 package model;
 
-public class ContaCorrente extends Conta{
+public class ContaCorrente extends Conta implements AssociadoVip{
+
+    private int qdeCotas;
+    public double lucro;
+
+    public double lucros(int qdeCotas, double valorCota){
+        return lucro;
+
+    }
+
+    public ContaCorrente() {
+    }
+
+    public ContaCorrente(int id, double saldo, int qdeCotas) {
+        super(id, saldo);
+        this.qdeCotas = qdeCotas;
+    }
+
+    public int getQdeCotas() {
+        return qdeCotas;
+    }
+
+    public void setQdeCotas(int qdeCotas) {
+        this.qdeCotas = qdeCotas;
+    }
+
+    @Override
+    public String toString() {
+        return "ContaCorrente{" +
+                "id=" + id +
+                ", saldo=" + saldo +
+                ", qdeCotas=" + qdeCotas +
+                '}';
+    }
+
 }
